b2633a595fd2e29cb43a0c026dad348b1a036f1c
Elena <149805926+ElenaKas8@users.noreply.github.com>
2024-03-10 19:52:51 +0100

    updated

--- maket.js
+++ maket.js
@@ -1,30 +1,63 @@
 
-    // Функция для изменения текста ссылки
-    // function changeLinkText() {
-    //     let links = document.querySelectorAll(".linkText");
-    //     links.forEach(link => {
-    //         if (window.innerWidth <= 803) {
-    //             link.textContent = "Все кейсы";
-    //         } else {
-    //             link.textContent = "ПОДРОБНЕЕ";
-    //         }
-    //     });
-    // }
-    
-    // // Вызываем функцию при загрузке страницы и при изменении размера окна
-    // window.onload = changeLinkText;
-    // window.onresize = changeLinkText;
-    
 
-    function changeLinkText() {
-        console.log("Function called"); // добавьте эту строку для проверки
-        let links = document.querySelectorAll(".linkText");
-        links.forEach(link => {
-            if (window.innerWidth <= 803) {
-                link.textContent = "Все кейсы";
-            } else {
-                link.textContent = "ПОДРОБНЕЕ";
-            }
-        });
+const frame = document.querySelector('.frame');
+const textSlidesContainer = document.querySelector('.hero-text.container');
+let slider_index = 0;
+
+const texts = [
+    'По любому вопросу',
+    'Мы решим вашу задачу',
+    'Сотрудники с опытом',
+    'Обращайтесь к нам'
+];
+
+const title = document.querySelector('.hero-text h1');
+
+const [leftBtn, rightBtn] = document.querySelectorAll('.triggers > button');
+
+rightBtn.onclick = () => {
+    if (slider_index !== texts.length - 1) {
+        slider_index++;
+    } else {
+        slider_index = 0;
     }
-    
+    updateText();
+};
+
+leftBtn.onclick = () => {
+    if (slider_index !== 0) {
+        slider_index--;
+    } else {
+        slider_index = texts.length - 1;
+    }
+    updateText();
+};
+
+const slider_btns = document.querySelector('.slider_btns');
+
+for (let i = 0; i < texts.length; i++) {
+    const round_btn = document.createElement('button');
+    round_btn.className = 'round_btn';
+    slider_btns.append(round_btn);
+
+    round_btn.onclick = () => {
+        slider_index = i;
+        updateText();
+    };
+}
+
+function updateText() {
+    title.textContent = texts[slider_index];
+    changeActiveBtn();
+}
+
+function changeActiveBtn() {
+    const all_btns = document.querySelectorAll('.round_btn');
+    all_btns.forEach((elem, index) => {
+        if (slider_index === index) {
+            elem.classList.add('active');
+        } else {
+            elem.classList.remove('active');
+        }
+    });
+}
