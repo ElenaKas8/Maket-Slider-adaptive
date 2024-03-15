d1d35b04993cc16a1598b660cef0e560b0288e3c
Elena <149805926+ElenaKas8@users.noreply.github.com>
2024-03-15 18:56:56 +0100

    с нами работают

--- maket.js
+++ maket.js
@@ -61,3 +61,64 @@ function changeActiveBtn() {
         }
     });
 }
+
+
+//-----------Slider2-----------------
+
+document.addEventListener('DOMContentLoaded', function () {
+    const frame2 = document.querySelector('.our-clients-content .hero-text');
+    const cards_container = frame2.querySelector('img');
+
+    let slider2_index = 0;
+
+    const images2 = [
+        'images/microsoft.png',
+        'images/portfol.png',
+        'images/buhone.png',
+    ];
+
+    // Добавляем первое изображение в cards_container
+    cards_container.src = images2[0];
+
+    const [leftButton, rightButton] = document.querySelectorAll('.our-clients-content .triggers2 button');
+
+    rightButton.onclick = () => {
+        slider2_index = (slider2_index + 1) % images2.length;
+        cards_container.src = images2[slider2_index];
+        changeActiveBtn2();
+    };
+
+    leftButton.onclick = () => {
+        slider2_index = (slider2_index - 1 + images2.length) % images2.length;
+        cards_container.src = images2[slider2_index];
+        changeActiveBtn2();
+    };
+
+    const slider2_btns = document.querySelector('.our-clients-content .slider2_btns');
+
+    // Добавляем кнопки выбора слайда
+    images2.forEach((_, i) => {
+        const round_btn = document.createElement('button');
+        round_btn.className = 'round_btn';
+        slider2_btns.appendChild(round_btn);
+
+        round_btn.onclick = () => {
+            slider2_index = i;
+            cards_container.src = images2[slider2_index];
+            changeActiveBtn2();
+        };
+    });
+
+    function changeActiveBtn2() {
+        const all_btns = document.querySelectorAll('.our-clients-content .round_btn');
+        all_btns.forEach((elem, index) => {
+            if (slider2_index === index) {
+                elem.classList.add('active');
+            } else {
+                elem.classList.remove('active');
+            }
+        });
+    }
+
+    changeActiveBtn2();
+});
