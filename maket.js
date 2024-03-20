14ecc5d77f66231fd8de328df1b717ccb6410d55
Elena <149805926+ElenaKas8@users.noreply.github.com>
2024-03-20 21:16:19 +0100

    slider1 updated

--- maket.js
+++ maket.js
@@ -1,67 +1,163 @@
 
 
-const frame = document.querySelector('.frame');
-const textSlidesContainer = document.querySelector('.hero-text.container');
-let slider_index = 0;
+// const frame = document.querySelector('.frame');
+// const textSlidesContainer = document.querySelector('.hero-text.container');
+// let slider_index = 0;
 
-const texts = [
-    'По любому вопросу',
-    'Мы решим вашу задачу',
-    'Сотрудники с опытом',
-    'Обращайтесь к нам'
-];
+// const texts = [
+//     'По любому вопросу',
+//     'Мы решим вашу задачу',
+//     'Сотрудники с опытом',
+//     'Обращайтесь к нам'
+// ];
 
-const title = document.querySelector('.hero-text h1');
+// const title = document.querySelector('.hero-text h1');
 
-const [leftBtn, rightBtn] = document.querySelectorAll('.triggers > button');
+// const [leftBtn, rightBtn] = document.querySelectorAll('.triggers > button');
 
-rightBtn.onclick = () => {
-    if (slider_index !== texts.length - 1) {
-        slider_index++;
-    } else {
-        slider_index = 0;
-    }
-    updateText();
-};
+// rightBtn.onclick = () => {
+//     if (slider_index !== texts.length - 1) {
+//         slider_index++;
+//     } else {
+//         slider_index = 0;
+//     }
+//     updateText();
+// };
 
-leftBtn.onclick = () => {
-    if (slider_index !== 0) {
-        slider_index--;
-    } else {
-        slider_index = texts.length - 1;
-    }
-    updateText();
-};
+// leftBtn.onclick = () => {
+//     if (slider_index !== 0) {
+//         slider_index--;
+//     } else {
+//         slider_index = texts.length - 1;
+//     }
+//     updateText();
+// };
 
-const slider_btns = document.querySelector('.slider_btns');
+// const slider_btns = document.querySelector('.slider_btns');
 
-for (let i = 0; i < texts.length; i++) {
-    const round_btn = document.createElement('button');
-    round_btn.className = 'round_btn';
-    slider_btns.append(round_btn);
+// for (let i = 0; i < texts.length; i++) {
+//     const round_btn = document.createElement('button');
+//     round_btn.className = 'round_btn';
+//     slider_btns.append(round_btn);
+
+//     round_btn.onclick = () => {
+//         slider_index = i;
+//         updateText();
+//     };
+// }
+
+// function updateText() {
+//     title.textContent = texts[slider_index];
+//     changeActiveBtn();
+// }
+
+// function changeActiveBtn() {
+//     const all_btns = document.querySelectorAll('.round_btn');
+//     all_btns.forEach((elem, index) => {
+//         if (slider_index === index) {
+//             elem.classList.add('active');
+//         } else {
+//             elem.classList.remove('active');
+//         }
+//     });
+// }
+
+
+
+
+const banner_container = document.querySelector('.banner.slider_container')
+const clients_container = document.querySelector('.clients.slider_container')
+const div_banner_round = document.querySelector('.banner_round_btns')
+const container = document.querySelector('.container')
+
+let banner_data = [
+    {text: 'Бухгалтерские услуги в вашем городе', btn_text: 'Наша презентация'},
+    {text: 'Всегда рады помочь', btn_text: 'Обращайтесь'},
+    {text: 'Бухгалтерские услуги от профессионалов', btn_text: 'Наши услуги'},
+    {text: 'Наши специалисты помогут Вам', btn_text: 'Наша презентация '},
+]
+let client_data = Array(8)
+
+let slide_width = container.clientWidth
+
+let slider_index = {
+    banner: 0,
+    clients: 0
+}
+
+for (let i = 0; i < banner_data.length; i++){
+    const div_container = document.createElement('div')
+    const h_text = document.createElement('h2')
+    const btn_elem = document.createElement('button')
+
+    div_container.className = 'banner_elem'
+    h_text.className = 'banner_text'
+    btn_elem.className = 'main_btn'
+
+    h_text.innerText = banner_data[i].text
+    btn_elem.innerText = banner_data[i].btn_text
+
+    div_container.append(h_text, btn_elem)
+    banner_container.append(div_container)
+
+    const round_btn = document.createElement('button')
+    div_banner_round.append(round_btn)
 
     round_btn.onclick = () => {
-        slider_index = i;
-        updateText();
-    };
+        slider_index.banner = i
+        banner_container.style.left = `${-1 * slider_index.banner * slide_width}px`
+        chnageActiveBtns()
+    }
 }
 
-function updateText() {
-    title.textContent = texts[slider_index];
-    changeActiveBtn();
+let [banner_left_btn, banner_right_btn] = document.querySelectorAll('.banner_triggers_btns > button')
+
+banner_right_btn.onclick = () => {
+    if (slider_index.banner !== banner_data.length - 1){
+        slider_index.banner++
+        banner_container.style.left = `${-1 * slider_index.banner * slide_width}px`
+    } else {
+        slider_index.banner = 0
+        banner_container.style.left = '0px'
+    }
+    chnageActiveBtns()
 }
 
-function changeActiveBtn() {
-    const all_btns = document.querySelectorAll('.round_btn');
-    all_btns.forEach((elem, index) => {
-        if (slider_index === index) {
-            elem.classList.add('active');
+banner_left_btn.onclick = () => {
+    if (slider_index.banner !== 0){
+        slider_index.banner--
+    } else {
+        slider_index.banner = banner_data.length - 1
+    }
+    banner_container.style.left = `${-1 * slider_index.banner * slide_width}px`
+    chnageActiveBtns()
+}
+
+for (let i = 0; i < client_data.length; i++){
+    const client_img = document.createElement('img')
+    client_img.src = './media/client.png'
+    clients_container.append(client_img)
+}
+
+
+
+
+function chnageActiveBtns(){
+    const all_btns = document.querySelectorAll('.banner_round_btns > button')
+    console.log(all_btns);
+    all_btns.forEach( (elem, index) => {
+        if (slider_index.banner === index){
+            elem.classList.add('active')
         } else {
-            elem.classList.remove('active');
+            elem.classList.remove('active')
         }
-    });
+    })
 }
 
+
+
+
+chnageActiveBtns()
 
 //-----------Slider2-----------------
 
