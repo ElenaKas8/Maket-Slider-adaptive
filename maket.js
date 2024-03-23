4d70c721b7306a8888b73ed67f77c0fd0ac84ace
Elena <149805926+ElenaKas8@users.noreply.github.com>
2024-03-23 10:27:05 +0100

    hover update

--- maket.js
+++ maket.js
@@ -1,98 +1,98 @@
 
 
-const banner_container = document.querySelector('.banner.slider_container')
- const clients_container = document.querySelector('.clients.slider_container')
-const div_banner_round = document.querySelector('.banner_round_btns')
-const container = document.querySelector('.container')
+// const banner_container = document.querySelector('.banner.slider_container')
+//  const clients_container = document.querySelector('.clients.slider_container')
+// const div_banner_round = document.querySelector('.banner_round_btns')
+// const container = document.querySelector('.container')
 
-let banner_data = [
-    {text: 'Бухгалтерские услуги в вашем городе', btn_text: 'Наша презентация'},
-    {text: 'Всегда рады помочь', btn_text: 'Обращайтесь'},
-    {text: 'Бухгалтерские услуги от профессионалов', btn_text: 'Наши услуги'},
-    {text: 'Наши специалисты помогут Вам', btn_text: 'Наша презентация '},
-]
- let client_data = Array(8)
+// let banner_data = [
+//     {text: 'Бухгалтерские услуги в вашем городе', btn_text: 'Наша презентация'},
+//     {text: 'Всегда рады помочь', btn_text: 'Обращайтесь'},
+//     {text: 'Бухгалтерские услуги от профессионалов', btn_text: 'Наши услуги'},
+//     {text: 'Наши специалисты помогут Вам', btn_text: 'Наша презентация '},
+// ]
+//  let client_data = Array(8)
 
-let slide_width = container.clientWidth
+// let slide_width = container.clientWidth
 
-let slider_index = {
-    banner: 0,
-    clients: 0
-}
+// let slider_index = {
+//     banner: 0,
+//     clients: 0
+// }
 
-for (let i = 0; i < banner_data.length; i++){
-    const div_container = document.createElement('div')
-    const h_text = document.createElement('h2')
-    const btn_elem = document.createElement('button')
+// for (let i = 0; i < banner_data.length; i++){
+//     const div_container = document.createElement('div')
+//     const h_text = document.createElement('h2')
+//     const btn_elem = document.createElement('button')
 
-    div_container.className = 'banner_elem'
-    h_text.className = 'banner_text'
-    btn_elem.className = 'main_btn'
+//     div_container.className = 'banner_elem'
+//     h_text.className = 'banner_text'
+//     btn_elem.className = 'main_btn'
 
-    h_text.innerText = banner_data[i].text
-    btn_elem.innerText = banner_data[i].btn_text
+//     h_text.innerText = banner_data[i].text
+//     btn_elem.innerText = banner_data[i].btn_text
 
-    div_container.append(h_text, btn_elem)
-    banner_container.append(div_container)
+//     div_container.append(h_text, btn_elem)
+//     banner_container.append(div_container)
 
-    const round_btn = document.createElement('button')
-    div_banner_round.append(round_btn)
+//     const round_btn = document.createElement('button')
+//     div_banner_round.append(round_btn)
 
-    round_btn.onclick = () => {
-        slider_index.banner = i
-        banner_container.style.left = `${-1 * slider_index.banner * slide_width}px`
-        chnageActiveBtns()
-    }
-}
+//     round_btn.onclick = () => {
+//         slider_index.banner = i
+//         banner_container.style.left = `${-1 * slider_index.banner * slide_width}px`
+//         chnageActiveBtns()
+//     }
+// }
 
-let [banner_left_btn, banner_right_btn] = document.querySelectorAll('.banner_triggers_btns > button')
+// let [banner_left_btn, banner_right_btn] = document.querySelectorAll('.banner_triggers_btns > button')
 
-banner_right_btn.onclick = () => {
-    if (slider_index.banner !== banner_data.length - 1){
-        slider_index.banner++
-        banner_container.style.left = `${-1 * slider_index.banner * slide_width}px`
-    } else {
-        slider_index.banner = 0
-        banner_container.style.left = '0px'
-    }
-    chnageActiveBtns()
-}
+// banner_right_btn.onclick = () => {
+//     if (slider_index.banner !== banner_data.length - 1){
+//         slider_index.banner++
+//         banner_container.style.left = `${-1 * slider_index.banner * slide_width}px`
+//     } else {
+//         slider_index.banner = 0
+//         banner_container.style.left = '0px'
+//     }
+//     chnageActiveBtns()
+// }
 
-banner_left_btn.onclick = () => {
-    if (slider_index.banner !== 0){
-        slider_index.banner--
-    } else {
-        slider_index.banner = banner_data.length - 1
-    }
-    banner_container.style.left = `${-1 * slider_index.banner * slide_width}px`
-    chnageActiveBtns()
-}
+// banner_left_btn.onclick = () => {
+//     if (slider_index.banner !== 0){
+//         slider_index.banner--
+//     } else {
+//         slider_index.banner = banner_data.length - 1
+//     }
+//     banner_container.style.left = `${-1 * slider_index.banner * slide_width}px`
+//     chnageActiveBtns()
+// }
 
-for (let i = 0; i < client_data.length; i++){
-    const client_img = document.createElement('img')
-    client_img.src = './images/microsoft.png'
-    clients_container.append(client_img)
-}
-
-
-function chnageActiveBtns(){
-    const all_btns = document.querySelectorAll('.banner_round_btns > button')
-    console.log(all_btns);
-    all_btns.forEach( (elem, index) => {
-        if (slider_index.banner === index){
-            elem.classList.add('active')
-        } else {
-            elem.classList.remove('active')
-        }
-    })
-}
+// for (let i = 0; i < client_data.length; i++){
+//     const client_img = document.createElement('img')
+//     client_img.src = './images/microsoft.png'
+//     clients_container.append(client_img)
+// }
 
 
+// function chnageActiveBtns(){
+//     const all_btns = document.querySelectorAll('.banner_round_btns > button')
+//     console.log(all_btns);
+//     all_btns.forEach( (elem, index) => {
+//         if (slider_index.banner === index){
+//             elem.classList.add('active')
+//         } else {
+//             elem.classList.remove('active')
+//         }
+//     })
+// }
 
 
-chnageActiveBtns()
 
-//-----------Slider2-----------------
+
+// chnageActiveBtns()
+
+// //-----------Slider2-----------------
 
 
 
@@ -135,6 +135,129 @@ chnageActiveBtns()
 
 
 // updateSliderPosition();
+
+
+
+
+
+
+// Для первого слайдера (banner)
+const banner_container = document.querySelector('.banner.slider_container');
+const div_banner_round = document.querySelector('.banner_round_btns');
+const container = document.querySelector('.container');
+
+let banner_data = [
+    {text: 'Бухгалтерские услуги в вашем городе', btn_text: 'Наша презентация'},
+    {text: 'Всегда рады помочь', btn_text: 'Обращайтесь'},
+    {text: 'Бухгалтерские услуги от профессионалов', btn_text: 'Наши услуги'},
+    {text: 'Наши специалисты помогут Вам', btn_text: 'Наша презентация'},
+];
+
+let slide_width = container.clientWidth;
+
+let slider_index = {
+    banner: 0,
+};
+
+for (let i = 0; i < banner_data.length; i++){
+    const div_container = document.createElement('div');
+    const h_text = document.createElement('h2');
+    const btn_elem = document.createElement('button');
+
+    div_container.className = 'banner_elem';
+    h_text.className = 'banner_text';
+    btn_elem.className = 'main_btn';
+
+    h_text.innerText = banner_data[i].text;
+    btn_elem.innerText = banner_data[i].btn_text;
+
+    div_container.append(h_text, btn_elem);
+    banner_container.append(div_container);
+
+    const round_btn = document.createElement('button');
+    div_banner_round.append(round_btn);
+
+    round_btn.onclick = () => {
+        slider_index.banner = i;
+        banner_container.style.left = `${-1 * slider_index.banner * slide_width}px`;
+        changeActiveBtns();
+    };
+}
+
+let [banner_left_btn, banner_right_btn] = document.querySelectorAll('.banner_triggers_btns > button');
+
+banner_right_btn.onclick = () => {
+    if (slider_index.banner !== banner_data.length - 1){
+        slider_index.banner++;
+        banner_container.style.left = `${-1 * slider_index.banner * slide_width}px`;
+    } else {
+        slider_index.banner = 0;
+        banner_container.style.left = '0px';
+    }
+    changeActiveBtns();
+};
+
+banner_left_btn.onclick = () => {
+    if (slider_index.banner !== 0){
+        slider_index.banner--;
+    } else {
+        slider_index.banner = banner_data.length - 1;
+    }
+    banner_container.style.left = `${-1 * slider_index.banner * slide_width}px`;
+    changeActiveBtns();
+};
+
+function changeActiveBtns(){
+    const all_btns = document.querySelectorAll('.banner_round_btns > button');
+    all_btns.forEach( (elem, index) => {
+        if (slider_index.banner === index){
+            elem.classList.add('active');
+        } else {
+            elem.classList.remove('active');
+        }
+    });
+}
+
+changeActiveBtns();
+
+// Для второго слайдера (clients)
+const clientsContainer = document.querySelector('.clients.slider_container');
+const leftButton = document.querySelector('.btn_slider.left');
+const rightButton = document.querySelector('.btn_slider.right');
+let clientIndex = 0;
+
+// Создание клиентских элементов
+const clientCount = 8;
+const slideWidth = clientsContainer.clientWidth;
+
+for (let i = 0; i < clientCount; i++) {
+    const clientImg = document.createElement('img');
+    clientImg.src = './images/microsoft.png';
+    clientsContainer.appendChild(clientImg);
+}
+
+// Функция для обновления позиции слайдера
+function updateSliderPosition() {
+    clientsContainer.style.transform = `translateX(-${clientIndex * slideWidth}px)`;
+}
+
+// Обработчик события для кнопки "Влево"
+leftButton.addEventListener('click', () => {
+    if (clientIndex > 1) {
+        clientIndex -= 2;
+        updateSliderPosition();
+    }
+});
+
+// Обработчик события для кнопки "Вправо"
+rightButton.addEventListener('click', () => {
+    if (clientIndex < clientCount - 2) {
+        clientIndex += 2;
+        updateSliderPosition();
+    }
+});
+
+updateSliderPosition();
 
 
 
